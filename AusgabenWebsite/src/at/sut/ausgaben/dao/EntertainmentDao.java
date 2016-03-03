fba5644b46737b0136ae8e8e740706297076339c
Tobias Sutterlüty <Tobias@Tobi_laptop>
2016-03-03 15:35:59 +0100

    entertainmentdao db connection

--- AusgabenWebsite/src/at/sut/ausgaben/dao/EntertainmentDao.java
+++ AusgabenWebsite/src/at/sut/ausgaben/dao/EntertainmentDao.java
@@ -1,5 +1,27 @@
 package at.sut.ausgaben.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class EntertainmentDao {
+	private static final String ipAddress = "192.168.157.131";
+	private static final String databaseName = "ausgaben";
+	private static final String userName = "ausgabenTobi";
+	private static final String password = "tobias";
+
+	public void insertAmount(int amount) throws Exception{
+		Connection connection = getConnection();
+		Statement stmt = connection.createStatement();
+	}
+	
+	
+	private Connection getConnection() throws Exception {
+		Class.forName("com.mysql.jdbc.Driver");
+		return DriverManager.getConnection("jdbc:mysql://" + ipAddress + "/"
+				+ databaseName + "?user=" + userName + "&password=" + password
+				+ "");
+	}
 
 }
